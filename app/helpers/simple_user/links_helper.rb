8af83248a650ae919b5b5bfaab32fb427c96b13f
Pablo Marti <pablo.marti89@gmail.com>
2013-03-19 14:47:51 -0600

    Change path on links helper

--- app/helpers/simple_user/links_helper.rb
+++ app/helpers/simple_user/links_helper.rb
@@ -2,7 +2,7 @@ module SimpleUser
   module LinksHelper
 
 	def login(label = "Login", options = {})
-		link_to label, new_user_session_path, options
+		link_to label, main_app.new_user_session_path, options
 	end
 	
   end
--- lib/simple_user/version.rb
+++ lib/simple_user/version.rb
@@ -1,3 +1,3 @@
 module SimpleUser
-  VERSION = "0.1.1.5"
+  VERSION = "0.1.1.6"
 end
