264193f8d7fd05e09bf17b29d64ebda4b2d84b93
Pablo Marti <pablo.marti89@gmail.com>
2013-05-30 14:32:05 -0600

    recover_password

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -52,6 +52,8 @@ module Dummy
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
