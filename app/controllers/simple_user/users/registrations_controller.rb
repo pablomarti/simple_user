2ef7b6be1c2bc0ffb19bf422c4a426fe3fcd1e19
Pablo Marti <pablo@greenmediaagency.com>
2013-07-18 11:48:20 -0600

    Change redirect after sign up

--- app/controllers/simple_user/users/registrations_controller.rb
+++ app/controllers/simple_user/users/registrations_controller.rb
@@ -1,5 +1,14 @@
 module SimpleUser
 	class Users::RegistrationsController < Devise::RegistrationsController
 
+		  def after_sign_up_path_for(resource)
+		  	if ENV['REDIRECT_USER_AFTER_SIGNIN'] == 'false' || !defined?(session[:return_to]) || session[:return_to] == "/" ||  session[:return_to].nil?
+		    	after_sign_in_path_for(resource)
+		    else
+		    	return_to = session[:return_to]
+		    	return_to
+		    end
+		  end
+
 	end
 end
